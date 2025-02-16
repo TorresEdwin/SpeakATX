cd4377848ad9a166a19d662f7641b6ffdf0da55f
Maya Lee <maya.v.lee393@gmail.com>
2025-02-15 20:54:17 -0600

    splash picture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <Router>
-            <Navbar />
+            <Navbar/>
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,12 +1,16 @@
 // Filename - pages/index.js
 
 import React from "react";
+import Picture from "../assets/picture.webp"
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to SpeakATX</h1>
-        </div>
+        <main className="splash-main">
+            <div className="splash-page">
+                <img src={Picture} className="splash-image"></img>
+                <h1 className="splash-title splash" >Welcome to SpeakATX</h1>
+            </div>
+        </main>
     );
 };
 
