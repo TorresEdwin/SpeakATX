a5e5f43b02802eecade0edcb58df63533471d0c7
steven <steven.zheng@utexas.edu>
2025-02-15 19:47:19 -0600

    basic about info

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -5,9 +5,26 @@ import React from "react";
 const About = () => {
     return (
         <div>
+            <link rel="stylesheet" href="about.css"></link>
             <h1>
-                About
+                About Us
             </h1>
+
+            SpeakATX is a website that aims to provide resources to <br/>minimal english and non-english speakers in Austin, TX.
+
+            <h2>
+                Team Members
+            </h2>
+
+            Maya Lee - @MayaLee393<br/>
+
+            Steven Zheng - @steven.zheng1<br/>
+
+            Amy Wu - @amypwu<br/>
+
+            Edwin Torres - @EdwinIsSad<br/>
+
+            Shawn Tran - @stran7365<br/>
         </div>
     );
 };
