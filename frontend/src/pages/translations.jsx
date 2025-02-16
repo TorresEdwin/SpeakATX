87911025fdf1a859e69c770365da102f809b0dc1
Steven Zheng <steven.zheng@utexas.edu>
2025-02-16 03:51:01 +0000

    Undo

--- frontend/src/pages/translations.jsx
+++ frontend/src/pages/translations.jsx
@@ -3,14 +3,6 @@
 import React from "react";
 
 const TranslationsPage = () => {
-    const buttonLabels = [
-        `Name: ${personInstance.name}, Age: ${personInstance.age}, Occupation: ${personInstance.occupation}`,
-        "Button 2",
-        "Button 3",
-        "Button 4",
-        "Button 5"
-      ];
-      
     return (
         <div>
             <h1>
