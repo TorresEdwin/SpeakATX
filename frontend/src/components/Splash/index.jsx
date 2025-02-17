b54e2be79284da9571da5e2d7c0263921a9d53a5
MayaLee393 <maya.v.lee393@gmail.com>
2025-02-16 21:30:41 -0600

    made splash component

---
+++ frontend/src/components/Splash/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+const Splash = () => {
+    return (
+        <>
+            <Container className="splash p-5 mb-4 bg-light">
+                <div className="splash-content">
+                    <h1 className="display-4">Welcome to SpeakATX</h1>
+                    <p className="lead">
+                        Providing resources 
+                        to minimal english and non-english speakers in Austin, TX.
+                    </p>
+                    <hr className="my-4" />
+                    <p>Local non-english 
+                    communities, translation services, and job postings.</p>
+                </div>
+            </Container>
+        </>
+    );
+};
+
+export default Splash;
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,7 +1,7 @@
 // Filename - pages/index.js
 
 import React from "react";
-import Picture from "../assets/picture.webp"
+import Splash from "../components/Splash"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
@@ -9,21 +9,20 @@ import { Container } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <main className="splash-main">
-            <Container className="splash p-5 mb-4 bg-light">
-                <div className="splash-content">
-                    <h1 className="display-4">Welcome to SpeakATX</h1>
-                    <p className="lead">
-                        Providing resources 
-                        to minimal english and non-english speakers in Austin, TX.
-                    </p>
-                    <hr className="my-4" />
-                    <p>Local non-english 
-                    communities, translation services, and job postings.</p>
-                </div>
+        <>
+           <main className="splash-main">
+                <Container className="splash p-5 mb-4">
+                    <div className="splash-content">
+                        <h1 className="display-4">Welcome to SpeakATX</h1>
+                        <p className="lead">
+                            Providing resources to minimal English and non-English speakers in Austin, TX.
+                        </p>
+                        <hr className="my-4" />
+                        <p>Local non-English communities, translation services, and job postings.</p>
+                    </div>
                 </Container>
-        </main>
-        
+            </main>
+        </>
     );
 };
 
