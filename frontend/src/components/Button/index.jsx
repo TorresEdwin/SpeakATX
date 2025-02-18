576cbcc6cceadedf22a160dea045a2d53331fc9c
Maya Lee <maya.v.lee393@gmail.com>
2025-02-17 20:18:12 -0600

    styled button

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -3,10 +3,21 @@ import { Link } from 'react-router-dom';
 const MyButton = ({ targetPage, buttonText }) => {
   return (
     <Link to={targetPage}>
-      <button className="btn btn-primary">
-        {buttonText}
-      </button>
-    </Link>
+  <button 
+    className="btn btn-primary" 
+    style={{
+      backgroundColor: "#bf5700",
+      outline: "none",
+      border: "none",
+      boxShadow: "none",
+      transition: "transform 0.1s ease-in-out",
+    }}
+    onMouseDown={(e) => e.target.style.transform = "scale(0.95)"}
+    onMouseUp={(e) => e.target.style.transform = "scale(1)"}
+  >
+    {buttonText}
+  </button>
+</Link>
   );
 };
 
