00ddad0bfde61eab1a85d612937f59448c958425
Shawn Tran <STran7365@gmail.com>
2025-02-19 17:52:45 -0600

    pie chart

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -18,6 +18,17 @@ const Home = () => {
                     <div className="content-container">
                     <Container >
                         <Row className="content text-center">
+                            <Col className="text-center">
+                                <h1 className="display-4">Coverage</h1>
+                                <p className="lead">
+                                There are many non-english speakers located in texas. Our website gathers info for these languages so far:
+                                </p>
+                                <hr className="my-4" />
+                            </Col>
+                            <Col className="d-none d-md-block"><img src="https://cdn.discordapp.com/attachments/1339072367799959592/1341915455526404096/jpeg.jpg?ex=67b7bba8&is=67b66a28&hm=b44244f327ed09492bb3d49b86b102c9a791db577f420d45f285e1a3a090a66b&" alt="Community Picture" className="img-fluid w-100" style={{ objectFit: "cover", maxHeight: "300px" }} /></Col>
+                        </Row>
+                        <hr style={{ borderTop: "4px solid rgb(68, 83, 71, 1)" }}></hr>
+                        <Row className="content text-center">
                             <Col className="d-none d-md-block"><img src="https://speakatx-images.s3.us-east-2.amazonaws.com/splash_page/translation.jpg" alt="Translation Picture" className="img-fluid w-100" style={{ objectFit: "cover", maxHeight: "300px" }}/></Col>
                             <Col>
                                 <h1 className="display-4">Multilingual Services</h1>
@@ -40,7 +51,6 @@ const Home = () => {
                             </Col>
                             <Col className="d-none d-md-block"><img src="https://speakatx-images.s3.us-east-2.amazonaws.com/splash_page/community.jpg" alt="Community Picture" className="img-fluid w-100" style={{ objectFit: "cover", maxHeight: "300px" }} /></Col>
                         </Row>
-
                         <hr style={{ borderTop: "4px solid rgb(68, 83, 71, 1)" }}></hr>
                         <Row className="content text-center">
                             <Col className="d-none d-md-block"><img src="https://speakatx-images.s3.us-east-2.amazonaws.com/splash_page/job.jpg" alt="Job Picture" className="img-fluid w-100" style={{ objectFit: "cover", maxHeight: "300px" }} /></Col>
