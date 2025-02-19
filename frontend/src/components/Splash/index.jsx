f9dcb56c2234d0db4c154faa9be44df7e1aa0211
Steven Zheng <steven.zheng@utexas.edu>
2025-02-19 17:29:04 -0600

    really bad but working translate embed

--- frontend/src/components/Splash/index.jsx
+++ frontend/src/components/Splash/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 
 const Splash = () => {
+    useEffect(() => {
+        // Dynamically load the Google Translate script
+        const script = document.createElement("script");
+        script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script.async = true;
+        document.body.appendChild(script);
+        
+        var duplicate_google_translate_counter = 0;//this stops google adding button multiple times
+        
+        // Initialize the Google Translate widget when the script is loaded
+        window.googleTranslateElementInit = () => {
+            new window.google.translate.TranslateElement({
+                pageLanguage: 'en', // Default language
+                includedLanguages: 'en,es,fr,de,it,pt', // List of available languages
+            }, 'google_translate_element');
+        };
+        
+        return () => {
+            // Clean up script when component unmounts
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <>
             
@@ -19,6 +42,8 @@ const Splash = () => {
                     services, and job postings
                 </p>
             </div>
+
+            <div id="google_translate_element"></div>
 
         </>
     );
