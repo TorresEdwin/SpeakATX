3f15339ef89b490f4b00dd5aa91f602a749d6844
Maya Lee <maya.v.lee393@gmail.com>
2025-02-19 17:08:14 -0600

    decreased gap on splash page

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -2,23 +2,23 @@ import { Link } from 'react-router-dom';
 
 const MyButton = ({ targetPage, buttonText }) => {
   return (
-    <Link to={targetPage}>
-  <button 
-    className="btn btn-primary" 
-    style={{
-      backgroundColor: "#bf5700",
-      outline: "none",
-      border: "none",
-      boxShadow: "none",
-      transition: "transform 0.1s ease-in-out",
-    }}
-    onMouseDown={(e) => e.target.style.transform = "scale(0.95)"}
-    onMouseUp={(e) => e.target.style.transform = "scale(1)"}
-  >
-    {buttonText}
-  </button>
-</Link>
-  );
+        <Link to={targetPage}>
+          <button 
+            className="btn btn-primary" 
+            style={{
+              backgroundColor: "#bf5700",
+              outline: "none",
+              border: "none",
+              boxShadow: "none",
+              transition: "transform 0.1s ease-in-out",
+            }}
+            onMouseDown={(e) => e.target.style.transform = "scale(0.95)"}
+            onMouseUp={(e) => e.target.style.transform = "scale(1)"}
+            >
+            {buttonText}
+          </button>
+        </Link>
+    );
 };
 
 export default MyButton;
