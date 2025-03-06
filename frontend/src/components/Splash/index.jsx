42359a53993559061c954b540d3ff268953d55dd
steven <steven.zheng@utexas.edu>
2025-03-05 18:01:18 -0600

    hide translate bar

--- frontend/src/components/Splash/index.jsx
+++ frontend/src/components/Splash/index.jsx
@@ -1,44 +1,40 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 
 const Splash = () => {
+    const googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement(
+            {
+                pageLanguage: "en",
+                autoDisplay: false
+            },
+            "google_translate_element"
+        );
+    };
     useEffect(() => {
-        // Dynamically load the Google Translate script
-        const script = document.createElement("script");
-        script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-        script.async = true;
-        document.body.appendChild(script);
-        
-        var duplicate_google_translate_counter = 0;//this stops google adding button multiple times
-        
-        // Initialize the Google Translate widget when the script is loaded
-        window.googleTranslateElementInit = () => {
-            new window.google.translate.TranslateElement({
-                pageLanguage: 'en', // Default language
-                includedLanguages: 'en,es,fr,de,it,pt', // List of available languages
-            }, 'google_translate_element');
-        };
-        
-        return () => {
-            // Clean up script when component unmounts
-            document.body.removeChild(script);
-        };
+        var addScript = document.createElement("script");
+        addScript.setAttribute(
+            "src",
+            "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        );
+        document.body.appendChild(addScript);
+        window.googleTranslateElementInit = googleTranslateElementInit;
     }, []);
+
 
     return (
         <>
-            
             <div className="splash-content">
                 <h1 className="display-4">SpeakATX</h1>
                 <p className="lead">
-                    Resources for minimal english 
+                    Resources for minimal english
                     and non-english speakers in Austin, TX
                 </p>
                 <hr className="my-4" />
                 <p>
-                    Local communities, translation 
+                    Local communities, translation
                     services, and job postings
                 </p>
             </div>
