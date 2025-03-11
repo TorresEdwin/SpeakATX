8b1e31000cc10fdf2b3a8509113c69cd42adbae2
Shawn Tran <STran7365@gmail.com>
2025-03-10 19:26:11 -0500

    disparate data

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -146,7 +146,7 @@ const About = () => {
             <p>SpeakATX aims to provide resources to minimal english and non-english speakers in Austin, TX. <br />
                 This site uses Yelp, Google Maps, and Glassdoor APIs. Made with React and Node.js.</p>
 
-            <p>We found that when integrating disparate data...</p>
+            <p>Integrating diverse sources of data has been an intersting challenge for us. We discovered how crucial it is to match individuals with the right language resources, as not all services cater to every linguistic need. Additionally, different communication challenges often require tailored solutions to ensure effective support. We also realized the importance of finding nearby organizations that offer a wide range of services, making sure that language support is accessible to all members of the Austin community. This process highlighted the significance of connecting individuals with the right resources that truly align with their unique needs.</p>
 
             <br />
 
