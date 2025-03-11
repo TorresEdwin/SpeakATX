4fd815dc799b57b56f9493c2b3c082a419e6e667
MayaLee393 <maya.v.lee393@gmail.com>
2025-03-11 04:54:50 +0000

    Edit app.py

--- backend/app.py
+++ backend/app.py
@@ -117,7 +117,7 @@ def get_translation_by_id(id):
         return jsonify(translation), 200
     return jsonify({"error": "Service not found"}), 404
 
-@app.route('/', methods=['GET'])
+@app.route('/health', methods=['GET'])
 def health_check():
     return 'It is OK', 200
 
